30c528daaa99f4cc0a31d461fd47d3d63778d4d0
Alif <alifrazanit@gmail.com>
2023-01-24 21:38:20 +0700

    63. Rendering List of Data

--- src/components/Expanse/Expanse.js
+++ src/components/Expanse/Expanse.js
@@ -6,19 +6,22 @@ import classes from './Expanse.module.css';
 export const Expanse = props => {
     const [filteredYear, setFilteredYear] = useState('2020');
     const onChangeFilter = selectedYear => {
-        console.log(selectedYear)
         setFilteredYear(selectedYear);
     }
 
 
-    return(
+    return (
         <Card className={classes['expanse']}>
-            <ExpanseFilter selected={filteredYear} onChangeFilter={onChangeFilter}/>
-            <ExpanseItem  
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            />
+            <ExpanseFilter selected={filteredYear} onChangeFilter={onChangeFilter} />
+            {props.items.map(item => (
+                <ExpanseItem
+                key={item.id}
+                    title={item.title}
+                    amount={item.amount}
+                    date={item.date}
+                />
+            ))}
+
         </Card>
     )
 }
