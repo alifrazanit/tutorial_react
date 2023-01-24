7efd6baa9d901a906f339023387626c77c85d7a7
Alif <alifrazanit@gmail.com>
2023-01-24 18:11:33 +0700

    36. Passing Data via props

--- src/components/ExpanseItem/ExpanseItem.js
+++ src/components/ExpanseItem/ExpanseItem.js
@@ -1,16 +1,12 @@
 import classes from './ExpanseItem.module.css';
 export const ExpanseItem = props => {
-    const ExDate = new Date(2023, 1, 24);
-    const ExTitle = 'Car Asuransi';
-    const ExAmount = 294.21;
-
 
     return(
         <div className={classes['expense-item']}>
-            <div>{ExDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className={classes['expense-item__description']}>
-                <h2>{ExTitle}</h2>
-                <div className={classes['expense-item__price']}>${ExAmount}</div>
+                <h2>{props.title}</h2>
+                <div className={classes['expense-item__price']}>${props.amount}</div>
             </div>
         </div>
     )
