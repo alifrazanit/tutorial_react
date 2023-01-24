c9f8795c8af89f3cbad1d72c816248caf7b593b7
Alif <alifrazanit@gmail.com>
2023-01-24 19:25:16 +0700

    48. Working with 'State'

--- src/components/ExpanseItem/ExpanseItem.js
+++ src/components/ExpanseItem/ExpanseItem.js
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import classes from './ExpanseItem.module.css';
 import { ExpanseDate } from '../ExpanseDate/ExpanseDate';
 import { Card } from '../Card/Card';
 export const ExpanseItem = props => {
+    const [title, setTitle]=useState(props.title);
+    const onChangeTitleHandler = e => {
+        setTitle('Updated')
+    }
 
     return(
         <Card className={classes['expense-item']}>
             <ExpanseDate date={props.date}/>
             <div className={classes['expense-item__description']}>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className={classes['expense-item__price']}>${props.amount}</div>
             </div>
+            <button onClick={onChangeTitleHandler}>Change title</button>
         </Card>
     )
 }
