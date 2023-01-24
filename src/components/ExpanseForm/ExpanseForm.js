850419c4795ef00ec12ebdca35617c6f703a84f6
Alif <alifrazanit@gmail.com>
2023-01-24 21:03:10 +0700

    53. Working with Multiple States

--- src/components/ExpanseForm/ExpanseForm.js
+++ src/components/ExpanseForm/ExpanseForm.js
@@ -1,19 +1,33 @@
 import classes from './ExpanseForm.module.css';
+import { useState } from 'react';
 export const ExpanseForm = props => {
+    const [title, setTitle] = useState('')
+    const [amount, setAmount] = useState('')
+    const [dateVal, setDate] = useState('')
+
+    const onChangeTitleHandler = e => {
+        setTitle(e.target.value);
+    }
+    const onChangeAmountHandler = e => {
+        setAmount(e.target.value);
+    }
+    const onChangeDateHandler = e => {
+        setDate(e.target.value);
+    }
     return (
         <form>
             <div className={classes['new-expense__controls']}>
                 <div className={classes['new-expense__control']}>
                     <label>Title</label>
-                    <input type='text' />
+                    <input type='text' onChange={onChangeTitleHandler} value={title}/>
                 </div>
                 <div className={classes['new-expense__control']}>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' />
+                    <input type='number' onChange={onChangeAmountHandler} min='0.01' step='0.01' value={amount}/>
                 </div>
                 <div className={classes['new-expense__control']}>
                     <label>Date</label>
-                    <input type='date' min='2020-01-01' max='2023-01-24' />
+                    <input type='date' onChange={onChangeDateHandler} min='2020-01-01' max='2023-01-24' value={dateVal}/>
                 </div>
             </div>
             <div className={classes['new-expense__actions']}>
