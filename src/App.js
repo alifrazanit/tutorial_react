80d92fc797430f6d97746935fc1ce2713cc65805
Alif <alifrazanit@gmail.com>
2023-01-29 21:45:13 +0700

    112

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, {useState}from 'react';
-import { UsersList } from './components/users-list/users-list';
-import { AddUser } from './components/users/add-user';
+import React, { useState } from 'react';
 
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  const [users, setUsers] = useState([])
-  const onAddUser = user => {
-    setUsers((prevData) => {
-      return [user, ...prevData]
-    })
-    console.log('user', user)
-  }
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div>
-      <AddUser onAddUser={onAddUser}/>
-      <UsersList users={users}/>
-    </div>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
