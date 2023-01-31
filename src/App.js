dac8d3e229b73aee01f610e8f2e6ee005e7de093
Alif <alifrazanit@gmail.com>
2023-01-31 09:55:41 +0700

    upd context : 122

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import { AuthContext } from './store/auth-context';
+
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  
-  
+
+
   const loginHandler = (email, password) => {
     setIsLoggedIn(true);
   };
@@ -18,13 +20,15 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{
+      isLoggedIn: isLoggedIn
+    }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,12 +1,15 @@
-import React from 'react';
-
+import React, { useContext} from 'react';
+import { AuthContext } from '../../store/auth-context';
 import Navigation from './Navigation';
 import classes from './MainHeader.module.css';
 
 const MainHeader = (props) => {
+  const ctx = useContext(AuthContext);
+
   return (
     <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
+      { ctx.isLoggedIn && <h1>Berhasil Login</h1>}
+      { !ctx.isLoggedIn && <h1>Gagal Login</h1>}
       <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
     </header>
   );
