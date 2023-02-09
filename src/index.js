1703f0ec9262418322b5dadb1288f283394ee4b8
alif <alifrazanit@gmail.com>
2023-02-09 16:40:47 +0700

    init section 11

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-import { AuthProvider } from './store/auth-context';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <AuthProvider>
-        <App />
-    </AuthProvider>
-);
+root.render(<App />);
