d7b33ffb8b228ca9333f9432904560ae8c68e334
alif <alifrazanit@gmail.com>
2023-02-09 16:38:08 +0700

    upd

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -26,6 +32,9 @@ const passwordReducer = (state, action) => {
 
 const Login = (props) => {
   const ctx = useContext(AuthContext);
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
   const [emailState, emailDispatch] = useReducer(emailReducer, {
     value: "",
     isValid: null,
@@ -74,13 +83,21 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    ctx.onLogin(emailState.value, passwordState.value);
+    if(formIsValid){
+      ctx.onLogin(emailState.value, passwordState.value);
+    } else if(!emailIsValid){
+      emailRef.current.focus();
+    } else if(!passwordIsValid){
+      emailRef.current.focus();
+    }
+
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailRef}
           type="email"
           id="email"
           value={emailState.value}
@@ -91,6 +108,7 @@ const Login = (props) => {
           htmlFor="email"
         />
         <Input
+          ref={passwordRef}
           type="password"
           id="password"
           value={passwordState.value}
@@ -101,7 +119,7 @@ const Login = (props) => {
           htmlFor="password"
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,9 +1,24 @@
 import classes from "./Input.module.css";
-import React from "react";
+import React, { useRef, useImperativeHandle } from "react";
 
 export const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+        focus: activate
+    }
+  })
   return (
-    <div className={`${classes.control} ${props.isValid === false ? classes.invalid : ""}`}>
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
       <label htmlFor={props.htmlFor}>{props.label}</label>
       <input
         ref={ref}
