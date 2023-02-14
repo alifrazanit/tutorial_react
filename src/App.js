e44015005ac08c0c506eb0d5460aca41cab1481f
alif <alifrazanit@gmail.com>
2023-02-14 14:08:48 +0700

    lewat dulu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import React, { useState, useCallback, useMemo } from 'react';
-import Button from './components/UI/Button/Button';
-
-import './App.css';
-import DemoList from './DemoList/DemoList';
+import Users from './components/Users';
 
 function App() {
-  const [listTitle, setListTitle] = useState('My List');
-  const changeTitleHandler = useCallback(() => {
-    setListTitle('New Title')
-  }, [])
-
-  const listItems= useMemo(() => [5,3,11,23], []);
-
   return (
-    <div className="app">
-      <DemoList title={listTitle} items={listItems}/>
-      <Button onClick={changeTitleHandler}>Change Title</Button><br/>
+    <div>
+      <Users />
     </div>
   );
 }
