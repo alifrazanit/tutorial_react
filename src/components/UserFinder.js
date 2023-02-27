5f600709e1969e98f2e23c2e35666e63a7f09902
Alif <alifrazanit@gmail.com>
2023-02-27 15:07:15 +0700

    '167'

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -3,20 +3,22 @@ import classes from './UserFinder.module.css';
 
 import Users from './Users';
 
-const DUMMY_USERS = [
-    { id: 'u1', name: 'Max' },
-    { id: 'u2', name: 'Manuel' },
-    { id: 'u3', name: 'Julie' },
-];
+
 
 export class UserFinder extends Component {
     constructor() {
         super();
         this.state = {
-            filteredUsers: DUMMY_USERS,
+            filteredUsers: [],
             searchTerm: ''
         }
     }
+
+    componentDidMount(){
+        this.setState({ filteredUsers: []})
+    }
+
+
     componentDidUpdate(prevProp, prevState) {
         if (prevState.searchTerm !== this.state.searchTerm) {
             this.setState({
@@ -41,29 +43,5 @@ export class UserFinder extends Component {
         );
     }
 }
-
-// const UserFinder = () => {
-//     const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
-//     const [searchTerm, setSearchTerm] = useState('');
-
-//     useEffect(() => {
-//         setFilteredUsers(
-//             DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
-//         );
-//     }, [searchTerm]);
-
-//     const searchChangeHandler = (event) => {
-//         setSearchTerm(event.target.value);
-//     };
-
-//     return (
-//         <Fragment>
-//             <div className={classes.finder}>
-//                 <input type='search' onChange={searchChangeHandler} />
-//                 <Users users={filteredUsers} />
-//             </div>
-//         </Fragment>
-//     );
-// };
 
 export default UserFinder;
